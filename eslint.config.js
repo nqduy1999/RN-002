0d1083a314a1a3b4ea635635742eb2710f25a90e
Duy Nguyn <duy.se.it99@gmail.com>
2026-04-17 12:18:36 +0700

    Refactor code structure for improved readability and maintainability

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  ...expoConfig,
+  {
+    ignores: ['dist/*', 'node_modules/*'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      'import/ignore': ['node_modules'],
+      'import/extensions': ['.js', '.jsx', '.json'],
+      'import/resolver': {
+        typescript: {
+          project: './jsconfig.json',
+          alwaysTryTypes: true,
+        },
+        node: {
+          extensions: ['.js', '.jsx', '.json'],
+        },
+      },
+    },
+  },
+]);
